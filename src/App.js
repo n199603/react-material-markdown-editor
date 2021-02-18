5cba42bd5ca847b72c2abdd8d5b25ea5e0cb7c82
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-02-18 09:28:11 +0900

    feat(app.js): add default state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return <div>Hello World</div>;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
   }
 }
 
